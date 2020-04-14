999aca9d3d84a7ab55005d464749cd8d32b10f8d
Vee Satayamas <vee.sa@protonmail.com>
2020-04-14 18:26:41 +0700

    Fix NS and style

--- pakt_clj/src/tennis.clj
+++ pakt_clj/src/tennis.clj
@@ -1,19 +1,10 @@
-(ns packt-clj.tennis
-
-  (:require
-
-   [clojure.data.csv :as csv]
-
-   [clojure.java.io :as io]
-
-   [semantic-csv.core :as sc]))
+(ns tennis
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]            
+            [semantic-csv.core :as sc]))
 
 (defn first-match [csv]
-
   (with-open [r (io/reader csv)]
-
     (->> (csv/read-csv r)
-
          sc/mappify
-
          first)))
